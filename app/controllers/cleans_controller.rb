496477ae68250e46f1893bd76811cbcda1cf9b82
satoko <ksm10arstaf@gmail.com>
2025-10-21 01:06:30 +0900

    refactor: CleansController内のメソッドに空行を追加して可読性を向上

--- app/controllers/cleans_controller.rb
+++ app/controllers/cleans_controller.rb
@@ -11,43 +11,58 @@ class CleansController < ApplicationController
     end
     
     def index
+
         @cleans = Clean.all
     end
     
     def new
+
         @clean = Clean.new
     end
     
     def create
+
         clean = Clean.new(clean_params)
         clean.user_id = current_user.id
+
         if clean.save!
+
             redirect_to :action => "index"
+
         else
+
             redirect_to :action => "new"
         end
     end
     
     def show
+
         @clean = Clean.find(params[:id])
         @comments = @clean.comments
         @comment = Comment.new
     end
 
     def edit
+
         @clean = Clean.find(params[:id])
     end
 
     def update
+
         clean = Clean.find(params[:id])
+        
         if clean.update(clean_params)
+
             redirect_to :action => "index"
+
         else
+            
             redirect_to :action => "edit"
         end
     end
 
     def destroy
+
         clean = Clean.find(params[:id])
         clean.destroy
         redirect_to action: :index
@@ -55,6 +70,7 @@ class CleansController < ApplicationController
     
     private
     def clean_params
+        
         params.require(:clean).permit(:maker, :genre, :cost, :name, :about, :image, :overall, :light, :power, :care, :time, :tag_ids)
     end
 end
